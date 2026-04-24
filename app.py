b47e1f6e08ecc01f0b7e1301099ea6d24a407a46
neetapyxeda <neeta.alex@pyxeda.ai>
2026-04-24 09:23:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.write("Hello Welcome!!!")
-st.image("size2.png")
+st.image("size2.PNG")
 num = int(st.text_input("Enter your favorite number:"))
 if num%2 == 0:
   st.write("It's even number")
